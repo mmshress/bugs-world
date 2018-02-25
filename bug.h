505b0a64884f4a42eb157c073dff713ef2ed0f47
tenfoldpaper <s.bien@jacobs-university.de>
2018-02-25 22:40:10 +0100

    Added header folder and bug.h

---
+++ bug.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   bug.h
+ * Author: whitebox
+ *
+ * Created on February 25, 2018, 10:39 PM
+ */
+
+#ifndef BUG_H
+#define BUG_H
+
+
+
+#endif /* BUG_H */
+
