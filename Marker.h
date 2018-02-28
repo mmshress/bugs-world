d31168bb0b104acb953372482a72542030d9c4f6
mohit <mohit@konnektiv.de>
2018-03-01 00:26:35 +0100

    Added and implemented Marker.h

--- Marker.h
+++ Marker.h
@@ -8,16 +8,74 @@
  * File:   Marker.h
  * Author: mohit
  *
- * Created on February 25, 2018, 10:47 PM
+ * Created on February 28, 2018, 11:21 PM
  */
 
 #ifndef MARKER_H
 #define MARKER_H
 
+#include "aux.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-
+    class Marker{ //TODO have code in the source file instead of the header file
+    private:
+        unsigned int bits;
+    public:
+        Marker(){
+            bits = 0; //set all bits to 0 at initialization
+        }
+        
+        void set_marker(aux::tmark mark, aux::tcolor color){
+            switch(color.c){
+                case 0: //black
+                    bits |= (1UL << mark.m); //sets bit mark.m from the right by shifting 0000000001 left by mark.m places
+                    break;
+                case 1://red
+                    bits |= (1UL << (mark.m + 6)); //sets bit (mark.m + 6) from the right
+                    break;
+            }
+        };
+        void clear_marker(aux::tmark mark, aux::tcolor color){
+            switch(color.c){
+                case 0://black
+                    bits &= ~(1UL << mark.m); //unsetting the bit mark.m from the right by shifting 000000001 left and negating/ AND gating
+                    break;
+                case 1://red
+                    bits &= ~(1UL << (mark.m + 6));
+                    break;
+            }
+            
+        };
+        bool check_marker(aux::tmark mark, aux::tcolor color){
+            int bit;
+            switch(color.c){
+                case 0://black
+                    bit = (bits >> mark.m) & 1U;
+                    return(bit == 1);
+                case 1://red
+                    bit = (bits >> (mark.m + 6)) & 1U;
+                    return (bit == 1);
+            }
+        };
+        bool check_other_marker(aux::tcolor color){
+            int bit;
+            switch(color.c){
+                case 0://black
+                    for(int i = 0; i < 5; i++){
+                        bit = (bits >> i) & 1U;
+                        if(bit == 1) return true;
+                    }
+                    return false;
+                case 1://red
+                    for(int i = 0; i < 5; i++){
+                        bit = (bits >> (i + 5)) & 1U;
+                        if(bit == 1) return true;
+                    }
+                    return false;
+            }
+        };
+    };
 
 
 
